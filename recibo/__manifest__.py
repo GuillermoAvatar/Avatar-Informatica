834714881ac864483973835a151ee2d2a26b2e8a
GuillermoAvatar <guillermo@avatarinformatica.com.ar>
2025-04-23 12:40:01 -0300

    Modulo Recibo

---
+++ recibo/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Reporte - Recibo Pagos',
+    'website': "https://www.avatar.com.py",
+    'version': '0.2',
+    'author': "Avatar Informaticca S.R.L.",
+    'depends': [
+        'account'
+    ],
+    'data': [
+        'views/reporte_recibo_pagos.xml'
+    ],
+    'installable': True,
+    'license': 'LGPL-3'
+}
